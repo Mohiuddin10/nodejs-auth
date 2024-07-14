d7ffd63d58454b1413c3f2652dc3562f97f27abb
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-07-14 23:32:51 +0600

    login user done

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,4 +1,5 @@
 const UsersAuth = require("../models/users.model");
+const bcrypt = require('bcryptjs');
 
 const loginUser = (req, res) => {
     res.render("login");
@@ -9,9 +10,17 @@ const userLoginWithPassword = async (req, res) => {
     const password = req.body.password;
     try {
         const matchUser = await UsersAuth.findOne({email});
-        console.log(matchUser);
+        if (matchUser) {
+            let matchPassword = await bcrypt.compare(password, matchUser.password);
+            matchPassword ? res.send({success: true,
+                message: "user valid",
+                data: matchUser
+            }) : res.send({success: false,
+                message: "user is not valid"
+            })
+        }
     } catch (error) {
-        
+        res.status(500).send({message: "something broken"})
     }
 }
 
