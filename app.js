13d723892b286163bc911554c18011ccd81fbe84
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-07-15 21:49:40 +0600

    working on passport session

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const ejs = require("ejs");
+const passport = require("passport");
+const MongoStore = require('connect-mongo');
+const session = require("express-session");
 const { router: usersRouter } = require("./router/users.router");
 const registerRoute = require("./router/register.route");
 const loginRoute = require("./router/login.route");
@@ -9,6 +12,23 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.set("view engine", "ejs");
+
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URL,
+    collectionName: "sessions",
+  })
+//   cookie: { secure: true }
+}))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const connectDB = () => {
     try {
